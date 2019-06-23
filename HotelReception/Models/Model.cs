f91db05966ca6afe1e0308e1143f8ddec435ca56
Dawid Polak <dawidpolak321@gmail.com>
2019-06-23 02:59:14 +0200

    Add tips to the project

--- HotelReception/Models/Model.cs
+++ HotelReception/Models/Model.cs
@@ -9,23 +9,29 @@ namespace HotelReception
 {
     class Model
     {
-        DBConnection database = DBConnection.Instance;
+        DBConnection database;
         public string UserName { get; set; }
         public bool Admin { get; set; }
 
         //do modelu maja dostep oba formy, wiec jak pierwszy wrzuci dane z logowania tutaj to drugi moze na nich pracowac
+        #region FormLogin
         public bool SetDatabaseAccess()
         {
             //polaczenie z baza
             //jesli sie udalo zwraca true
-            return true;
+            database = DBConnection.Instance;
+            if (database.IsConnected) return true; else return false;
+
         }
 
-        #region FormLogin
+        
         public bool Login(string login,string password)
         {
             //sprawdzenie danych logowania
             //jesli sie udalo zwraca true
+
+            //tutaj trzeba zrobic selecta szukajacego loginu i sprawdic przypisane do niego haslo
+            //sprawdzamy czy znalazlo login i czy haslo jest dobre (i zwracamy true/false)
             if (login == "admin" && password == "admin")
             {
                 Console.WriteLine(database.IsConnected);
@@ -37,9 +43,71 @@ namespace HotelReception
             else return false;
         }
         #endregion
-        
-        #region FormReception
 
+        #region FormReception
+        //UU   UU WW       WW    AAA      GGGG      AAA
+        //UU   UU WW       WW   AAAAA    GG  GG    AAAAA
+        //UU   UU WW   W   WW  AA   AA  GG        AA   AA
+        //UU   UU  WW WWW WW   AAAAAAA  GG   GG   AAAAAAA
+        // UUUUU     WW WW     AA   AA   GGGGGG   AA   AA
+        //narazie dalem void bo bedzie trzeba to jako instancje klasy Rooms raczej wyslac a jej jeszcze nie ma albo Tuple
+
+        //dodatkowo trzeba zrobic wyzwalacz ktory czy insercie i selectcie bedzie pilnowal wielkosci znakow( imiona i nazwiska z duzej reszta z malej)
+        //myslalem o czyms takim co by usuwalo wynajmy starszy niz miesiac zeby baza nie puchła w nieskonczonosc 
+        public void SelectRooms(int ileOsob, DateTime odKiedy, DateTime dokiedy, bool czyMaTaras)
+        {
+            //metoda ma zwracac liste pokoi ze wszystkimi parametrami spelniajace warunki z argumentow
+            
+        }
+        public void SelectRooms()
+        {
+            //metoda ma zwraca wszystkie pokoje
+        }
+        public void SelectRent()
+        {
+            //zwraca wszsytkie wynajmy
+        }
+        public void SelectEmployee()
+        {
+            //metoda ma zwracac wszystkich pracownikow 
+        }
+        public void InsertRoom(int ileOsob,int ilePoje, int ilePodw,bool czyTaras, int koszt)
+        {
+            //dodanie rooma 
+            
+        }
+        public void UpdateRoom(int id,int ileOsob, int ilePoje, int ilePodw, bool czyTaras, int koszt)
+        {
+            //cala reszta chyba wiadomo o co chodzi 
+        }
+        public void DeleteRoom(int id)
+        {
+
+        }
+        public void InsertRent(DateTime start, DateTime end, int idRoom, int idWorker, string imie, string nazwisko, string tel)
+        {
+
+        }
+        public void UpdateRent(int id, DateTime start, DateTime end, int idRoom, int idWorker, string imie, string nazwisko, string tel)
+        {
+            //koszt sie zmieni w zaleznosci od cilosci dni
+        }
+        public void DeleteRent(int id)
+        {
+
+        }
+        public void InsertEmployee(string imie, string nazwisko, bool isAdmin, string phone, string login, string pass)
+        {
+
+        }
+        public void UpdateEmployee(int id, string imie, string nazwisko, bool isAdmin, string phone, string login, string pass)
+        {
+            //koszt sie zmieni w zaleznosci od cilosci dni
+        }
+        public void DeleteEmployee(int id)
+        {
+
+        }
         #endregion
         private string HashPassword(string password)
         {
